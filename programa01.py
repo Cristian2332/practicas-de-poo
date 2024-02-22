695bd1043178dc6cf3cfb07dcb454ba88ed8b997
Cristian Peralta <156873105+Cristian2332@users.noreply.github.com>
2024-02-22 07:27:10 -0600

    Create programa01.py

---
+++ programa01.py
@@ -0,0 +1,14 @@
+"""
+Nombre: Cristian Peralta Aviles
+Matricula: 1722110292
+Grupo: TI21
+Fecha: 18/01/2024
+Aqui se aprendio sobre unos errores en python
+"""
+class A: #Define la clase
+    matricula = None #Define la variable matricula
+    nombre = None #Define la variable nombre
+
+objetoA=A() #Crea un objeto de la clase A
+
+print(objetoA.nombre) #Imprime los datos
