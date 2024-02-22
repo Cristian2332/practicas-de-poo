f36998815e232b28c36b69fdb4184ea1c9dd3a63
Cristian Peralta <156873105+Cristian2332@users.noreply.github.com>
2024-02-22 07:26:29 -0600

    Create programa00.py

---
+++ programa00.py
@@ -0,0 +1,8 @@
+"""
+Nombre: Cristian Peralta Aviles
+Matricula: 1722110292
+Grupo: TI21
+Fecha: 18/01/2024
+En este apartado aprendimos a utilizar la funcion print que nos ayuda a imprimir lo que tengamos
+"""
+print("Hola mundo Python!!") #Imprime el mensaje
